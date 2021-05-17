f7285d569f6034c13944bce8c156c1e8cc3b3d1c
Darric-Lu <darric.lu@gmail.com>
2021-05-17 22:46:45 +0800

    Add Works table page

---
+++ src/data/works.js
@@ -0,0 +1,242 @@
+export const works = [
+  {
+    id: 1,
+    title: "work01",
+    picture: require('./../image/works (1).png')
+  },
+  {
+    id: 2,
+    title: "work02",
+    picture: require('./../image/works (2).png')
+  },
+  {
+    id: 3,
+    title: "work03",
+    picture: require('./../image/works (3).png')
+  },
+  {
+    id: 4,
+    title: "work04",
+    picture: require('./../image/works (4).png')
+  },
+  {
+    id: 5,
+    title: "work05",
+    picture: require('./../image/works (5).png')
+  },
+  {
+    id: 6,
+    title: "work06",
+    picture: require('./../image/works (6).png')
+  },
+  {
+    id: 7,
+    title: "work07",
+    picture: require('./../image/works (7).png')
+  },
+  {
+    id: 8,
+    title: "work08",
+    picture: require('./../image/works (8).png')
+  },
+  {
+    id: 9,
+    title: "work09",
+    picture: require('./../image/works (9).png')
+  },
+  {
+    id: 10,
+    title: "work10",
+    picture: require('./../image/works (10).png')
+  },
+  {
+    id: 11,
+    title: "work11",
+    picture: require('./../image/works (11).png')
+  },
+  {
+    id: 12,
+    title: "work12",
+    picture: require('./../image/works (12).png')
+  },
+  {
+    id: 13,
+    title: "work13",
+    picture: require('./../image/works (13).png')
+  },
+  {
+    id: 14,
+    title: "work14",
+    picture: require('./../image/works (14).png')
+  },
+  {
+    id: 15,
+    title: "work15",
+    picture: require('./../image/works (15).png')
+  },
+  {
+    id: 16,
+    title: "work16",
+    picture: require('./../image/works (16).png')
+  },
+  {
+    id: 17,
+    title: "work17",
+    picture: require('./../image/works (17).png')
+  },
+  {
+    id: 18,
+    title: "work18",
+    picture: require('./../image/works (18).png')
+  },
+  {
+    id: 19,
+    title: "work19",
+    picture: require('./../image/works (19).png')
+  },
+  {
+    id: 20,
+    title: "work20",
+    picture: require('./../image/works (20).png')
+  },
+  {
+    id: 21,
+    title: "work21",
+    picture: require('./../image/works (21).png')
+  },
+  {
+    id: 22,
+    title: "work22",
+    picture: require('./../image/works (22).png')
+  },
+  {
+    id: 23,
+    title: "work23",
+    picture: require('./../image/works (23).png')
+  },
+  {
+    id: 24,
+    title: "work24",
+    picture: require('./../image/works (24).png')
+  },
+  {
+    id: 25,
+    title: "work25",
+    picture: require('./../image/works (25).png')
+  },
+  {
+    id: 26,
+    title: "work26",
+    picture: require('./../image/works (26).png')
+  },
+  {
+    id: 27,
+    title: "work27",
+    picture: require('./../image/works (27).png')
+  },
+  {
+    id: 28,
+    title: "work28",
+    picture: require('./../image/works (28).png')
+  },
+  {
+    id: 29,
+    title: "work29",
+    picture: require('./../image/works (29).png')
+  },
+  {
+    id: 30,
+    title: "work30",
+    picture: require('./../image/works (30).png')
+  },
+  {
+    id: 31,
+    title: "work031",
+    picture: require('./../image/works (31).png')
+  },
+  {
+    id: 32,
+    title: "work32",
+    picture: require('./../image/works (32).png')
+  },
+  {
+    id: 33,
+    title: "work33",
+    picture: require('./../image/works (33).png')
+  },
+  {
+    id: 34,
+    title: "work34",
+    picture: require('./../image/works (34).png')
+  },
+  {
+    id: 35,
+    title: "work35",
+    picture: require('./../image/works (35).png')
+  },
+  {
+    id: 36,
+    title: "work36",
+    picture: require('./../image/works (36).png')
+  },
+  {
+    id: 37,
+    title: "work37",
+    picture: require('./../image/works (37).png')
+  },
+  {
+    id: 38,
+    title: "work38",
+    picture: require('./../image/works (38).png')
+  },
+  {
+    id: 39,
+    title: "work39",
+    picture: require('./../image/works (39).png')
+  },
+  {
+    id: 40,
+    title: "work40",
+    picture: require('./../image/works (40).png')
+  },
+  {
+    id: 41,
+    title: "work41",
+    picture: require('./../image/works (41).png')
+  },
+  {
+    id: 42,
+    title: "work42",
+    picture: require('./../image/works (42).png')
+  },
+  {
+    id: 43,
+    title: "work43",
+    picture: require('./../image/works (43).png')
+  },
+  {
+    id: 44,
+    title: "work44",
+    picture: require('./../image/works (44).png')
+  },
+  {
+    id: 45,
+    title: "work45",
+    picture: require('./../image/works (45).png')
+  },
+  {
+    id: 46,
+    title: "work46",
+    picture: require('./../image/works (46).png')
+  },
+  {
+    id: 47,
+    title: "work01",
+    picture: require('./../image/works (47).png')
+  },
+  {
+    id: 48,
+    title: "work01",
+    picture: require('./../image/works (48).png')
+  },
+]
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,9 +1,105 @@
 <template>
-  <div class="works">WORKS</div>
+  <div class="works-area">
+    <div class="works-wrarpper">
+      <div class="work" v-for="work in works" :key="work.id">
+        <picture>
+          <img class="work-img" :src="work.picture" :alt="work.title" />
+        </picture>
+      </div>
+    </div>
+    <!-- Button trigger modal -->
+    <!-- <button
+      type="button"
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
+      Launch demo modal
+    </button> -->
+
+    <!-- Modal -->
+    <!-- <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>-->
+  </div>
 </template>
 
+<style lang="scss" scoped>
+@import "../assets/scss/main.scss";
+.works-area {
+  background-color: $subColor;
+  padding: 36px 0;
+}
+.works-wrarpper {
+  width: 90vw;
+  margin: 0 auto;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 40px;
+  background-color: $subColor;
+
+  .work {
+    min-width: 300px;
+    background-color: #ffffff;
+    -webkit-box-shadow: 0px 1px 3px 2px rgba(87, 87, 87, 0.2);
+    -moz-box-shadow: 0px 1px 3px 2px rgba(87, 87, 87, 0.2);
+    box-shadow: 0px 1px 3px 2px rgba(87, 87, 87, 0.2);
+    overflow: hidden;
+
+    &-img {
+      object-fit: cover;
+      width: 100%;
+    }
+  }
+}
+</style>
 <script>
+import { works } from "./../data/works";
 export default {
   name: "Works",
+  data() {
+    return {
+      works: [],
+    };
+  },
+  created() {
+    this.fecthWorksData();
+  },
+  methods: {
+    fecthWorksData() {
+      this.works = [...works];
+    },
+  },
 };
 </script>
