ee17b709a7d441c08a23b6d35a972d6c4a049e12
Darric-Lu <darric.lu@gmail.com>
2021-06-13 03:51:23 +0800

    Add works page ux & optimization

--- src/data/works.js
+++ src/data/works.js
@@ -2,241 +2,251 @@ export const works = [
   {
     id: 1,
     title: "work01",
-    picture: require('./../image/works (1).png')
+    picture: require('./../image/01.png')
   },
   {
     id: 2,
     title: "work02",
-    picture: require('./../image/works (2).png')
+    picture: require('./../image/02.png')
   },
   {
     id: 3,
     title: "work03",
-    picture: require('./../image/works (3).png')
+    picture: require('./../image/03.png')
   },
   {
     id: 4,
     title: "work04",
-    picture: require('./../image/works (4).png')
+    picture: require('./../image/04.png')
   },
   {
     id: 5,
     title: "work05",
-    picture: require('./../image/works (5).png')
+    picture: require('./../image/05.png')
   },
   {
     id: 6,
     title: "work06",
-    picture: require('./../image/works (6).png')
+    picture: require('./../image/06.png')
   },
   {
     id: 7,
     title: "work07",
-    picture: require('./../image/works (7).png')
+    picture: require('./../image/07.png')
   },
   {
     id: 8,
     title: "work08",
-    picture: require('./../image/works (8).png')
+    picture: require('./../image/08.png')
   },
   {
     id: 9,
     title: "work09",
-    picture: require('./../image/works (9).png')
+    picture: require('./../image/09.png')
   },
   {
     id: 10,
     title: "work10",
-    picture: require('./../image/works (10).png')
+    picture: require('./../image/10.png')
   },
   {
     id: 11,
     title: "work11",
-    picture: require('./../image/works (11).png')
+    picture: require('./../image/11.png')
   },
   {
     id: 12,
     title: "work12",
-    picture: require('./../image/works (12).png')
+    picture: require('./../image/12.png')
   },
   {
     id: 13,
     title: "work13",
-    picture: require('./../image/works (13).png')
+    picture: require('./../image/13.png')
   },
   {
     id: 14,
     title: "work14",
-    picture: require('./../image/works (14).png')
+    picture: require('./../image/14.png')
   },
   {
     id: 15,
     title: "work15",
-    picture: require('./../image/works (15).png')
+    picture: require('./../image/15.png')
   },
   {
     id: 16,
     title: "work16",
-    picture: require('./../image/works (16).png')
+    picture: require('./../image/16.png')
   },
   {
     id: 17,
     title: "work17",
-    picture: require('./../image/works (17).png')
+    picture: require('./../image/17.png')
   },
   {
     id: 18,
     title: "work18",
-    picture: require('./../image/works (18).png')
+    picture: require('./../image/18.png')
   },
   {
     id: 19,
     title: "work19",
-    picture: require('./../image/works (19).png')
+    picture: require('./../image/19.png')
   },
   {
     id: 20,
     title: "work20",
-    picture: require('./../image/works (20).png')
+    picture: require('./../image/20.png')
   },
   {
     id: 21,
     title: "work21",
-    picture: require('./../image/works (21).png')
+    picture: require('./../image/21.png')
   },
   {
     id: 22,
     title: "work22",
-    picture: require('./../image/works (22).png')
+    picture: require('./../image/22.png')
   },
   {
     id: 23,
     title: "work23",
-    picture: require('./../image/works (23).png')
+    picture: require('./../image/23.png')
   },
   {
     id: 24,
     title: "work24",
-    picture: require('./../image/works (24).png')
+    picture: require('./../image/24.png')
   },
   {
     id: 25,
     title: "work25",
-    picture: require('./../image/works (25).png')
+    picture: require('./../image/25.png')
   },
   {
     id: 26,
     title: "work26",
-    picture: require('./../image/works (26).png')
+    picture: require('./../image/26.png')
   },
   {
     id: 27,
     title: "work27",
-    picture: require('./../image/works (27).png')
+    picture: require('./../image/27.png')
   },
   {
     id: 28,
     title: "work28",
-    picture: require('./../image/works (28).png')
+    picture: require('./../image/28.png')
   },
   {
     id: 29,
     title: "work29",
-    picture: require('./../image/works (29).png')
+    picture: require('./../image/29.png')
   },
   {
     id: 30,
     title: "work30",
-    picture: require('./../image/works (30).png')
+    picture: require('./../image/30.png')
   },
   {
     id: 31,
     title: "work031",
-    picture: require('./../image/works (31).png')
+    picture: require('./../image/31.png')
   },
   {
     id: 32,
     title: "work32",
-    picture: require('./../image/works (32).png')
+    picture: require('./../image/32.png')
   },
   {
     id: 33,
     title: "work33",
-    picture: require('./../image/works (33).png')
+    picture: require('./../image/33.png')
   },
   {
     id: 34,
     title: "work34",
-    picture: require('./../image/works (34).png')
+    picture: require('./../image/34.png')
   },
   {
     id: 35,
     title: "work35",
-    picture: require('./../image/works (35).png')
+    picture: require('./../image/35.png')
   },
   {
     id: 36,
     title: "work36",
-    picture: require('./../image/works (36).png')
+    picture: require('./../image/36.png')
   },
   {
     id: 37,
     title: "work37",
-    picture: require('./../image/works (37).png')
+    picture: require('./../image/37.png')
   },
   {
     id: 38,
     title: "work38",
-    picture: require('./../image/works (38).png')
+    picture: require('./../image/38.png')
   },
   {
     id: 39,
     title: "work39",
-    picture: require('./../image/works (39).png')
+    picture: require('./../image/39.png')
   },
   {
     id: 40,
     title: "work40",
-    picture: require('./../image/works (40).png')
+    picture: require('./../image/40.png')
   },
   {
     id: 41,
     title: "work41",
-    picture: require('./../image/works (41).png')
+    picture: require('./../image/41.png')
   },
   {
     id: 42,
     title: "work42",
-    picture: require('./../image/works (42).png')
+    picture: require('./../image/42.png')
   },
   {
     id: 43,
     title: "work43",
-    picture: require('./../image/works (43).png')
+    picture: require('./../image/43.png')
   },
   {
     id: 44,
     title: "work44",
-    picture: require('./../image/works (44).png')
+    picture: require('./../image/44.png')
   },
   {
     id: 45,
     title: "work45",
-    picture: require('./../image/works (45).png')
+    picture: require('./../image/45.png')
   },
   {
     id: 46,
     title: "work46",
-    picture: require('./../image/works (46).png')
+    picture: require('./../image/46.png')
   },
   {
     id: 47,
-    title: "work01",
-    picture: require('./../image/works (47).png')
+    title: "work47",
+    picture: require('./../image/47.png')
   },
   {
     id: 48,
-    title: "work01",
-    picture: require('./../image/works (48).png')
+    title: "work48",
+    picture: require('./../image/48.png')
+  },
+  {
+    id: 49,
+    title: "work48",
+    picture: require('./../image/49.png')
+  },
+  {
+    id: 50,
+    title: "work48",
+    picture: require('./../image/50.png')
   },
 ]
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,45 +1,78 @@
 <template>
   <div class="works-area">
     <div class="works-wrarpper">
-      <div class="work" data-aos="fade-up" v-for="work in works" :key="work.id">
+      <div
+        class="work"
+        data-aos="fade-up"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        v-for="work in works"
+        :key="work.id"
+        @click="modalImageChange(work.id)"
+      >
         <picture>
           <img class="work-img" :src="work.picture" :alt="work.title" />
         </picture>
       </div>
     </div>
-    <!-- Button trigger modal -->
-    <!-- <button
-      type="button"
-      class="btn btn-primary"
-      data-bs-toggle="modal"
-      data-bs-target="#exampleModal"
-    >
-      Launch demo modal
-    </button> -->
-
     <!-- Modal -->
-    <!-- <div
+    <div
       class="modal fade"
       id="exampleModal"
       tabindex="-1"
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-xl">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <div class="modal-body">
             <button
               type="button"
               class="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
-          </div>
+            <div class="modal-image-wrapper">
+              <picture>
+                <img
+                  class="modal-image-work"
+                  :src="modal.image"
+                  :alt="modal.number"
+                />
+              </picture>
+            </div>
+            <div class="right-btn" @click="modalImageChange(modal.next)">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                class="bi bi-caret-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"
+                />
+              </svg>
+            </div>
+            <div class="left-btn" @click="modalImageChange(modal.previous)">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                class="bi bi-caret-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
-    </div>-->
+    </div>
   </div>
 </template>
 
@@ -66,13 +99,58 @@
     -webkit-box-shadow: 0px 1px 3px 2px rgba(87, 87, 87, 0.2);
     -moz-box-shadow: 0px 1px 3px 2px rgba(87, 87, 87, 0.2);
     box-shadow: 0px 1px 3px 2px rgba(87, 87, 87, 0.2);
+    border: 20px solid transparent;
     overflow: hidden;
 
     &-img {
       object-fit: cover;
       width: 100%;
     }
+    &:hover {
+      cursor: pointer;
+      border: 0.1px solid transparent;
+    }
   }
+}
+.modal {
+  &-body {
+    position: relative;
+
+    .left-btn,
+    .right-btn {
+      position: absolute;
+      top: 50%;
+      line-height: 45px;
+      width: 48px;
+      height: 48px;
+      &:hover {
+        width: 48px;
+        height: 48px;
+        background-color: $subColor;
+        border-radius: 50%;
+      }
+    }
+    .left-btn {
+      left: 5%;
+    }
+    .right-btn {
+      right: 5%;
+    }
+  }
+  &-image {
+    &-wrapper {
+      // height: 90vh;
+      overflow: hidden;
+    }
+    &-work {
+      object-fit: cover;
+      width: 100%;
+    }
+  }
+}
+.btn-close {
+  position: absolute;
+  right: 15px;
 }
 </style>
 <script>
@@ -84,6 +162,12 @@ export default {
   data() {
     return {
       works: [],
+      modal: {
+        number: "",
+        image: "",
+        next: "",
+        previous: "",
+      },
     };
   },
   created() {
@@ -96,6 +180,32 @@ export default {
   methods: {
     fecthWorksData() {
       this.works = [...works];
+    },
+    modalImageChange(workId) {
+      this.modal.number = "";
+      this.modal.image = "";
+      this.works.map((work) => {
+        if (work.id === workId) {
+          this.modal.number = work.id;
+          this.modal.image = work.picture;
+        }
+      });
+      this.changeNext(workId);
+      console.log("next", this.modal.next);
+      this.changePrevious(workId);
+      console.log("previous", this.modal.previous);
+    },
+    changeNext(workId) {
+      if (Number(workId) === this.works.length) {
+        return (this.modal.next = 1);
+      }
+      this.modal.next = Number(workId) + 1;
+    },
+    changePrevious(workId) {
+      if (Number(workId) - 1 === 0) {
+        return (this.modal.previous = 48);
+      }
+      this.modal.previous = Number(workId) - 1;
     },
   },
 };
